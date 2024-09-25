e7091d2cb066a8a637cd13a69c7d1d971a2c3460
AbdouMr1 <jhonmith47@gmail.com>
2024-09-25 22:31:08 +0200

    to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn ecommbackend.wsgi:application --log-file -
--- ecommbackend/settings.py
+++ ecommbackend/settings.py
@@ -1,21 +1,22 @@
 import os
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-6@mkfzo7=(-sc8r@cl^h(t$m%1^%7fe!1rj^$67jn32k7be6_5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+import dj_database_url
+import os
 
+DATABASES = {
+    "default": dj_database_url.parse("postgres://stivo_user:xvxSXcHFzinX9jckHHFGhMZcnSxoPKbC@dpg-crnfcdo8fa8c738f976g-a.oregon-postgres.render.com/stivo")
+}
 
 
 INSTALLED_APPS = [
@@ -28,6 +29,7 @@ INSTALLED_APPS = [
     'cloth',
     'rest_framework',
     'corsheaders',
+    'django_heroku',
 ]
 
 
@@ -52,6 +54,9 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React development server URL
     "https://pn4g26ts-3000.uks1.devtunnels.ms",
+    "https://66ede2f3cfd22b7228abd2f5--cheery-alfajores-26ddb5.netlify.app",
+    "https://stivio.netlify.app",
+
 ]
 
 ROOT_URLCONF = 'ecommbackend.urls'
@@ -124,7 +129,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -132,6 +137,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Add these settings to handle media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# ALLOWED_HOSTS = ['Stivio.herokuapp.com']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    "default": dj_database_url.parse("postgres://stivo_user:xvxSXcHFzinX9jckHHFGhMZcnSxoPKbC@dpg-crnfcdo8fa8c738f976g-a.oregon-postgres.render.com/stivo")
+}
